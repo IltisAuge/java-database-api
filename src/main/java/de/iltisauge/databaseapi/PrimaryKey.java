076bbfafb620e4e0e82b2e6da055149e180a481d
IltisAuge <danielziegler.1@gmx.de>
2024-03-07 17:23:21 +0100

    Add PrimaryKey class

---
+++ src/main/java/de/iltisauge/databaseapi/PrimaryKey.java
@@ -0,0 +1,11 @@
+package de.iltisauge.databaseapi;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Getter
+public class PrimaryKey {
+
+	private final String value;
+}
